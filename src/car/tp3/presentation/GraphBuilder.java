d5731298aa29011fc2e21cf0452067cc523fdd38
eliottbricout <bricouteliott@gmail.com>
2017-03-27 20:08:05 +0200

    graphbuild

--- src/car/tp3/presentation/GraphBuilder.java
+++ src/car/tp3/presentation/GraphBuilder.java
@@ -14,15 +14,21 @@ public class GraphBuilder {
 		this.properties = properties;
 	}
 	
-	public void build(MySystemServer system) throws IOException{
+	public void build(Client client) throws IOException{
 		BufferedReader reader = new BufferedReader(new FileReader(properties));
 		String line;
 		String[] split;
 		while(!(line = reader.readLine()).equals("--Actor--") && line != null ){
 			split = line.split(" ");
-			if(split.length > 2){				
-				system = split[0];
-				address = split[1].split(":");
+			if(split.length > 3){			
+				for(int i = 3;i<split.length;i++)
+					client.interprete("create "+split[0]+" "+split[1]+" "+split[2]+" "+split[i]);
+			}
+		}
+		
+		while((line = reader.readLine()) != null){
+			if(line.split(" ").length == 2){
+				client.interprete("link "+line);
 			}
 		}
 		
